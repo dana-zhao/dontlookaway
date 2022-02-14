96b580497994884bd7f0b7dcee9536c1719aaa45
danazhao1 <dana.zhao@mail.utoronto.ca>
2022-02-14 08:21:26 -0800

    Character Movement

--- GameV1/Assets/Scripts/CharacterMove.cs
+++ GameV1/Assets/Scripts/CharacterMove.cs
@@ -4,74 +4,110 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    private float _playerInputH;
-    private float _playerInputV;
+    //private float _playerInputH;
+    //private float _playerInputV;
     private float _rotationInputH;
     private float _rotationInputV;
     private Vector3 _userRot;
-    private bool _userJumped;
-    private bool _isGrounded;
+    //private bool _userJumped;
+    //private bool _isGrounded;
+    private float _minRotV;
+    private float _maxRotV;
 
     [SerializeField]
     public AudioSource pickupsfx;
 
     private const float _horizontalSpeed = 2.0f;
     private const float _verticalSpeed = 2.0f;
-    private const float _inputScale = 0.2f;
-    private const float _jumpScale = 3.0f;
+    //private const float _inputScale = 0.2f;
+    //private const float _jumpScale = 3.0f;
+    private const float _maxTiltAngle = 20.0f;
     private int objectsPickedUp = 0;
 
-    private Rigidbody _rigidbody;
+    public float Speed = 4f;
+    public float JumpHeight = 2f;
+    public float Gravity = -9.81f;
+    //public LayerMask Ground;
+
+    private CharacterController _controller;
+    private Vector3 _velocity;
+    private Vector3 _moveDirection;
+    private bool _isGrounded = true;
+
+    //private Rigidbody _rigidbody;
     private Transform _transform;
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        //_rigidbody = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
+        _userRot = _transform.rotation.eulerAngles;
+        _minRotV = _userRot.x - _maxTiltAngle;
+        _maxRotV = _userRot.x + _maxTiltAngle;
+
+        _controller = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        _playerInputV = Input.GetAxis("Vertical");
-        _playerInputH = Input.GetAxis("Horizontal");
+        //_playerInputV = Input.GetAxis("Vertical");
+        //_playerInputH = Input.GetAxis("Horizontal");
         _rotationInputH = Input.GetAxis("Mouse X");
         _rotationInputV = Input.GetAxis("Mouse Y");
-        _userJumped = Input.GetButton("Jump");
+        //_userJumped = Input.GetButton("Jump");
+
+        _isGrounded = _controller.isGrounded;
+        if (_isGrounded && _velocity.y < 0)
+            _velocity.y = 0f;
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _moveDirection = transform.TransformDirection(move);
+        _controller.Move(_moveDirection * Time.deltaTime * Speed);
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+            _velocity.y += Mathf.Sqrt(JumpHeight * -2.0f * Gravity);
+
+        _velocity.y += Gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
-        _userRot = _transform.rotation.eulerAngles;
+
         _userRot += new Vector3(0, _rotationInputH * _horizontalSpeed, 0);
+        _userRot -= new Vector3(_rotationInputV * _verticalSpeed, 0, 0);
+        _userRot.x = Mathf.Clamp(_userRot.x, _minRotV, _maxRotV);
 
         _transform.rotation = Quaternion.Euler(_userRot);
-        _rigidbody.velocity += _transform.forward * _playerInputV * _inputScale;
-        _rigidbody.velocity += _transform.right * _playerInputH * _inputScale;
+        //_rigidbody.MovePosition(_rigidbody.position + _transform.forward * _playerInputV * _inputScale * Time.fixedDeltaTime + _transform.right * _playerInputH * _inputScale * Time.fixedDeltaTime)
+        //_rigidbody.velocity += _transform.forward * _playerInputV * _inputScale;
+        //_rigidbody.velocity += _transform.right * _playerInputH * _inputScale;
 
-        if (_isGrounded && _userJumped)
-        {
-            _userJumped = false;
-            _rigidbody.AddForce(Vector3.up * _jumpScale, ForceMode.Impulse);
+        //if (_isGrounded && _userJumped)
+        //{
+            //_userJumped = false;
+            //_rigidbody.AddForce(Vector3.up * _jumpScale, ForceMode.Impulse);
             
-        }
+        //}
     }
 
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            _isGrounded = true;
-        }
-    }
+    //void OnCollisionEnter(Collision other)
+    //{
+        //if (other.gameObject.tag == "Ground")
+        //{
+            //_isGrounded = true;
+        //}
+    //}
 
-    void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            _isGrounded = false;
-        }
-    }
+    //void OnCollisionExit(Collision other)
+    //{
+        //if (other.gameObject.tag == "Ground")
+        //{
+            //_isGrounded = false;
+        //}
+    //}
 
     // What to do when we collide with an object marked for pickup with the "Pick Up" tag
     void OnTriggerEnter(Collider other)
