9dba8625cf8ccad6cb6f5463edb1761dc74da869
Hayden McGowan <hayden.patrick.mcgowan@gmail.com>
2022-02-17 22:42:13 -0500

    Several Audio Additions and Changes to Gift Shop
    Key PU SFX
    
    Temp Foosteps
    
    Temp Music (using Main Hall BGM instead of Gift Shop
    
    Reverb Zone
    
    Ghost Idle SFX
    
    Dynamic Audio Prox 3D
    
    Other changes

---
+++ GameV1/Assets/Scripts/TempFootsteps.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempFootsteps : MonoBehaviour
+{
+    public float stepRate = 0.8f;
+    public float stepCoolDown;
+    public AudioSource sfx_TempFootStep;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        stepCoolDown -= Time.deltaTime;
+        if ((Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) && stepCoolDown < 0f)
+        {
+            sfx_TempFootStep.pitch = 1f + Random.Range(-0.2f, 0.2f);
+            sfx_TempFootStep.Play();
+            stepCoolDown = stepRate;
+        }
+    }
+}
