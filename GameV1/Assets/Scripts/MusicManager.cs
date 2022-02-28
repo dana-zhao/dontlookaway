841e864a057ad6b14582284cdfc9abd7e90f1192
Hayden McGowan <hayden.patrick.mcgowan@gmail.com>
2022-02-28 02:23:22 -0500

    music/implementation adjustments, new sfx assets

---
+++ GameV1/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixerSnapshot fader;
+    [SerializeField]
+    private AudioMixer specmusicfade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fader.TransitionTo(2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
